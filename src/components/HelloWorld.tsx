b4ae66d63844fa71c81b21a0e3c791f798f916cf
Igor Wandermurem Dummer <igor.dummer@industriall.ai>
2024-05-27 14:00:57 -0300

    refactor: :recycle: Change home page

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { Block, Button } from "vcc-ui";
+import { Text } from "vcc-ui";
 import { useCars } from "../rooks/useCars";
 import { CarCard } from "./CarCard";
+
+import styles from "../../public/css/home.module.css";
+import { Spacer } from "./Spacer";
 
 export const HelloWorld: React.FC = () => {
   const { cars } = useCars();
   
   return (
-    <Block extend={{ padding: 20 }}>
-      {cars.map((car) => (
-        <CarCard key={car.id} car={car} />
-      ))}
-    </Block>
+    <div className={styles.homeWrapper}>
+      <Text variant="cook">Todos os modelos Recharge</Text>
+      <Spacer></Spacer>
+      <div className={styles.cardsWrapper} id="card-list">
+        {cars.map(car => <CarCard key={car.id} car={car}/>)}
+      </div>
+      {/* <PaginationDesktop onClickLeft={() => onClickNavigate(true)} onClickRight={() => onClickNavigate(false)}/>
+      <PaginationMobile selected={selected} onClick={onClickMobile} total={cars.length}></PaginationMobile> */}
+    </div>
   );
 };
