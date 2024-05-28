9930ff84f174a5726825ada30c011deb3073e5f1
Igor Wandermurem Dummer <igor.dummer@industriall.ai>
2024-05-28 08:41:41 -0300

    refactor: :recycle: Change home page logic

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,48 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Text } from "vcc-ui";
 import { useCars } from "../hooks/useCars";
 import { CarCard } from "./CarCard";
-
-import styles from '../../public/css/home.module.css'
+import styles from '../../public/css/home.module.css';
 import { Spacer } from "./Spacer";
 import PaginationDesktop from "./PaginationDesktop";
 import PaginationMobile from "./PaginationMobile";
 
 export const HomeComponent: React.FC = () => {
   const { cars } = useCars();
-  const [selected, setSelected ]= useState(0)
+  const [selected, setSelected] = useState(0);
+  const cardListRef = useRef<HTMLDivElement>(null);
 
   const onClickNavigate = (left: boolean) => {
-    let cardList = document.getElementById("card-list");
-    let card = cardList?.firstElementChild;
-    let cardSize = (card?.clientWidth ?? 0) + 24;
-    let scrollSize = cardList?.scrollWidth ?? 0;
-    let scrollPosition = cardList?.scrollLeft ?? 0;
+    const cardList = cardListRef.current;
+    const card = cardList?.firstElementChild as HTMLElement;
+    const cardSize = (card?.clientWidth ?? 0) + 24;
+    const scrollPosition = cardList?.scrollLeft ?? 0;
 
-    if(left) cardList?.scrollTo({ left: scrollPosition - cardSize })
-    else cardList?.scrollTo({ left: scrollPosition + cardSize })
-  }
+    cardList?.scrollTo({ left: scrollPosition + (left ? -cardSize : cardSize), behavior: 'smooth' });
+  };
 
   const onClickMobile = (index: number) => {
-    let cardList = document.getElementById("card-list");
-    let card = cardList?.firstElementChild;
-    let cardSize = (card?.clientWidth ?? 0) + 24;
-    let scrollSize = cardList?.scrollWidth ?? 0;
-    let scrollPosition = cardList?.scrollLeft ?? 0;
+    const cardList = cardListRef.current;
+    const card = cardList?.firstElementChild as HTMLElement;
+    const cardSize = (card?.clientWidth ?? 0) + 24;
 
-    cardList?.scrollTo({ left: index * cardSize})
+    cardList?.scrollTo({ left: index * cardSize, behavior: 'smooth' });
     setSelected(index);
-  }
+  };
 
   return (
     <div className={styles.homeWrapper}>
       <Text variant="cook">Todos os modelos Recharge</Text>
-      <Spacer></Spacer>
-      <div className={styles.cardsWrapper} id="card-list">
-        {cars.map(car => <CarCard key={car.id} car={car}/>)}
+      <Spacer />
+      <div className={styles.cardsWrapper} ref={cardListRef} id="card-list">
+        {cars.map(car => <CarCard key={car.id} car={car} />)}
       </div>
-      <PaginationDesktop onClickLeft={() => onClickNavigate(true)} onClickRight={() => onClickNavigate(false)}/>
-      <PaginationMobile selected={selected} onClick={onClickMobile} total={cars.length}></PaginationMobile>
+      <PaginationDesktop onClickLeft={() => onClickNavigate(true)} onClickRight={() => onClickNavigate(false)} />
+      <PaginationMobile selected={selected} onClick={onClickMobile} total={cars.length} />
     </div>
   );
 };
